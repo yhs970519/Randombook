10f0980782235e04b2c06ddf610272be69f045e1
yhs970519 <yhs970519@naver.com>
2022-02-14 23:01:45 +0900

    87th commit

--- Randombook.js
+++ Randombook.js
@@ -3,7 +3,7 @@
 //document.write("<script type='text/javascript' src='https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.1/p5.min.js'></script>");
 
 //'img+숫자' 변수 생성//
-let bookN = 11;
+let bookN = 10;
 for (let i = 1; i < bookN; i++) {
 	eval("let img" + i + ";");
 }
