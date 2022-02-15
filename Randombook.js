9f55a544bb29e1fcaf6cb29784c1ef2c4dd64d1c
yhs970519 <yhs970519@naver.com>
2022-02-15 21:28:55 +0900

    137th commit

--- Randombook.js
+++ Randombook.js
@@ -37,10 +37,12 @@ function draw() {
 function randombook1() {
 	let a = Math.floor(Math.random() * img.length);
 	image(img[a], 50, 50, 200, 300);
-	if(img.length > 0.1) {
+	if(img.length > 0) {
 		img.splice(a, 1);
+		console.log(img.length);
 	}else if(img.length <= 0) {
 		img.splice(0, 0, img1, img2, img3, img4, img5, img6, img7, img8, img9, img10);
+		console.log(img.length);
 	}
 }
 
