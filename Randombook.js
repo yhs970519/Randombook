cadd651061d7cf0d2d7dbf5cac42b7994986074d
yhs970519 <yhs970519@naver.com>
2022-02-15 17:35:41 +0900

    116th commit

--- Randombook.js
+++ Randombook.js
@@ -1,21 +1,21 @@
 // JavaScript Document
 
-//'img+숫자' 변수 생성//
+//'img+数字' 変数　生成//
 let bookN = 11;
 for (let i = 1; i < bookN; i++) {
 	eval("let img" + i + ";");
 }
 
-//'img' 배열 생성//
+//'img' 配列　生成//
 let img = [];
 
 function preload(){
 	
 	for (let i = 1; i < bookN; i++) {
-		//'img+숫자'에 순서대로 이미지 링크 할당하는 변수 생성//
+		//'img+数字'に 順番にイメージリンクを当てる。//
 		let j = i - 1;
 		eval("img" + i + " = loadImage('book/book" + i + ".jpg');");
-		//'img'배열에 순서대로 'img+숫자' 변수 할당
+		// 'img'配列に 'img+数字' 変数を当てる。//
 		eval("img[" + j + "] = img" + i + ";");
 	}
 }
@@ -25,23 +25,11 @@ function setup()  {
 	background(0, 0, 0, 20);
 }
 
-// function draw() {
-// 	//'bookimg'는 'img' 배열 속 값을 랜덤으로 섞는 변수//
-// 	let bookimg = random(img);
-// 	image(bookimg, 50, 50, 200, 300);
-// 	image(bookimg, windowWidth - 250, 50, 200, 300);
-// 	image(bookimg, 50, windowHeight - 350, 200, 300);
-// 	image(bookimg, windowWidth - 250, windowHeight - 350, 200, 300);
-
-// 	fill('powderblue');
-// 	rect(mouseX - 75, mouseY - 75, 150);
-// }
-
 function draw() {
 	let a = img.length - 0.001;
 	let b = Math.floor(random(a));
 	let c = Math.floor(random(a));
-	for(let i = 1; i < 61; i++) {
+	for(let i = 1; i < 121; i++) {
 		if(i % 60 === 0) {
 			image(img[b], mouseX, mouseY, 200, 300);
 			image(img[c], 50, 50, 200, 300);
