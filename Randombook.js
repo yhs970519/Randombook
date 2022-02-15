4194ed804ac05c8635ca6ad81c14e678034bdb95
yhs970519 <yhs970519@naver.com>
2022-02-15 17:30:29 +0900

    115th commit

--- Randombook.js
+++ Randombook.js
@@ -41,17 +41,15 @@ function draw() {
 	let a = img.length - 0.001;
 	let b = Math.floor(random(a));
 	let c = Math.floor(random(a));
-	for(let i = 1; i < 65; i++) {
-		if(i % 60 !== 0) {
+	for(let i = 1; i < 61; i++) {
+		if(i % 60 === 0) {
+			image(img[b], mouseX, mouseY, 200, 300);
+			image(img[c], 50, 50, 200, 300);
+		}else {
 			continue;
 			image(img[b], mouseX, mouseY, 200, 300);
 			image(img[c], 50, 50, 200, 300);
 		}
-		// else {
-		// 	continue;
-		// 	image(img[b], mouseX, mouseY, 200, 300);
-		// 	image(img[c], 50, 50, 200, 300);
-		// }
 	// let c = img.length - 0.001;
 	// for (let a = c; a > 5; a--) {
 	// 	let b = Math.floor(random(a));
