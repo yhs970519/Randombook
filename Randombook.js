b6e7629b56e97dfbe7c0b2aebc0727645d9aebda
yhs970519 <yhs970519@naver.com>
2022-02-25 06:43:14 +0900

    179th commit

--- Randombook.js
+++ Randombook.js
@@ -291,23 +291,47 @@ window.addEventListener('DOMContentLoaded', function(){
 
 	if( is_mobile ) {
 		// translate images in mobile
-		document.addEventListener("touchstart", (e) => {
-			mouseX = -e.touches[0].clientX * 2.5 / window.innerWidth;
-			mouseY = -e.touches[0].clientY * 10 / window.innerHeight;
-			main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
-		});
-		document.addEventListener("touchmove", (e) => {
-			mouseX = -e.changedTouches[0].clientX * 2.5 / window.innerWidth;
-			mouseY = -e.changedTouches[0].clientY * 10 / window.innerHeight;
-			main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
-			// console.log(mouseX);
-			// console.log(mouseY);
-		});
-		document.addEventListener("touchend", (e) => {
-			mouseX = -e.changedTouches[0].clientX * 2.5 / window.innerWidth;
-			mouseY = -e.changedTouches[0].clientY * 10 / window.innerHeight;
-			main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
-		});
+		function touchHandler(event) {
+			var touch = event.changedTouches[0];
+		
+			var simulatedEvent = document.createEvent("MouseEvent");
+				simulatedEvent.initMouseEvent({
+				touchstart: "mousedown",
+				touchmove: "mousemove",
+				touchend: "mouseup"
+			}[event.type], true, true, window, 1,
+				touch.screenX, touch.screenY,
+				touch.clientX, touch.clientY, false,
+				false, false, false, 0, null);
+		
+			touch.target.dispatchEvent(simulatedEvent);
+			event.preventDefault();
+		}
+		
+		function init() {
+			document.addEventListener("touchstart", touchHandler, true);
+			document.addEventListener("touchmove", touchHandler, true);
+			document.addEventListener("touchend", touchHandler, true);
+			document.addEventListener("touchcancel", touchHandler, true);
+		}
+
+		// document.addEventListener("touchstart", (e) => {
+		// 	mouseX = -e.touches[0].clientX * 2.5 / window.innerWidth;
+		// 	mouseY = -e.touches[0].clientY * 10 / window.innerHeight;
+		// 	main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
+		// });
+		// document.addEventListener("touchmove", (e) => {
+		// 	mouseX = -e.changedTouches[0].clientX * 2.5 / window.innerWidth;
+		// 	mouseY = -e.changedTouches[0].clientY * 10 / window.innerHeight;
+		// 	main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
+		// 	// console.log(mouseX);
+		// 	// console.log(mouseY);
+		// });
+		// document.addEventListener("touchend", (e) => {
+		// 	mouseX = -e.changedTouches[0].clientX * 2.5 / window.innerWidth;
+		// 	mouseY = -e.changedTouches[0].clientY * 10 / window.innerHeight;
+		// 	main_img.style.transform = 'translate(' + mouseX + '%, ' + mouseY + '%)';
+		// });
 	} else {
 		// tranlate images in pc
 		document.addEventListener("mousemove", (e) => {
@@ -319,29 +343,7 @@ window.addEventListener('DOMContentLoaded', function(){
 		});
 	}
 
-	// function touchHandler(event) {
-	// 	var touch = event.changedTouches[0];
 	
-	// 	var simulatedEvent = document.createEvent("MouseEvent");
-	// 		simulatedEvent.initMouseEvent({
-	// 		touchstart: "mousedown",
-	// 		touchmove: "mousemove",
-	// 		touchend: "mouseup"
-	// 	}[event.type], true, true, window, 1,
-	// 		touch.screenX, touch.screenY,
-	// 		touch.clientX, touch.clientY, false,
-	// 		false, false, false, 0, null);
-	
-	// 	touch.target.dispatchEvent(simulatedEvent);
-	// 	event.preventDefault();
-	// }
-	
-	// function init() {
-	// 	document.addEventListener("touchstart", touchHandler, true);
-	// 	document.addEventListener("touchmove", touchHandler, true);
-	// 	document.addEventListener("touchend", touchHandler, true);
-	// 	document.addEventListener("touchcancel", touchHandler, true);
-	// }
 
 	// book images array//////////////////////////////////////////////////
 	let arrayN = 31; //+1
