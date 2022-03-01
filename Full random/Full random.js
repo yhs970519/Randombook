c0527b3d7920bd9ff89143d9a174495fef93a94c
yhs970519 <yhs970519@naver.com>
2022-03-01 18:12:20 +0900

    216th commit

--- Full random/Full random.js
+++ Full random/Full random.js
@@ -2,8 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', function(){
     let ract = document.querySelector(".ract");
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
     ract.style.width = Math.sqrt(2) * 100 + 'px';
     ract.style.height = 100 + 'px';
+    ract.style.backgroundColor = 'rgba(' + red + ', ' + green + ', ' + blue + ')';
 
     let circle1 = document.querySelector(".circle1");
     let circle2 = document.querySelector(".circle2");
@@ -13,4 +17,11 @@ window.addEventListener('DOMContentLoaded', function(){
     circle1.style.left = leftRandom + 'px';
     circle2.style.top = topRandom - 10 + 'px';
     circle2.style.left = leftRandom + 15 + 'px';
+
+    let text1 = document.querySelector(".text1");
+    let text2 = document.querySelector(".text2");
+    let text3 = document.querySelector(".text3");
+    text1.style.left = Math.sqrt(2) * 110 + 'px';
+    text2.style.left = Math.sqrt(2) * 110 + 'px';
+    text3.style.left = Math.sqrt(2) * 110 + 'px';
 })
