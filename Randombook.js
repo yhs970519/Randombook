7cb20ef048ed2f8da762ead2dc5c902a3ea259fc
yhs970519 <yhs970519@naver.com>
2022-03-03 17:19:39 +0900

    227th commit

--- Randombook.js
+++ Randombook.js
@@ -330,6 +330,7 @@ window.addEventListener('DOMContentLoaded', function(){
 	let frameR = document.querySelector(".frame_right");
 	let mouseX;
 	let mouseY;
+	let header = document.querySelector("header");
 
 	if( is_mobile ) {
 		// translate images in mobile
@@ -343,6 +344,8 @@ window.addEventListener('DOMContentLoaded', function(){
 		$(".mainImg").draggable({
 			containment: ".mobilescreen"
 		});
+
+		header.style.transform = 'scale(65%)';
 
 		// document.addEventListener("touchstart", (e) => {
 		// 	mouseX = -e.touches[0].clientX * 2.5 / window.innerWidth;
@@ -370,6 +373,8 @@ window.addEventListener('DOMContentLoaded', function(){
 			// console.log(mouseX);
 			// console.log(mouseY);
 		});
+
+		header.style.transform = 'scale(75%)';
 	}
 
 	// book images array//////////////////////////////////////////////////
@@ -582,7 +587,7 @@ window.addEventListener('DOMContentLoaded', function(){
 	let aside_ul = document.querySelector(".aside");
 	let textSpace1 = document.getElementsByClassName("CT");
 	let textSpace2 = document.getElementsByClassName("ct");
-	let header = document.querySelector("header");
+	// let header = document.querySelector("header");
 	let logo = document.querySelector(".logo");
 	let TEXT = document.querySelector(".TEXT");
 	let text1 = document.querySelector(".text1");
